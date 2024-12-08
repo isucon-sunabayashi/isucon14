90b29bea30667dc643886ef50d55243370dc8737
Takazumi Okibayashi <takazumi.okibayashi@crowdworks.co.jp>
2024-12-08 15:29:04 +0900

    idx_ride_statuses_ride_id_chair_sent_at_created_at

--- scripts/create-index.sh
+++ scripts/create-index.sh
@@ -22,8 +22,8 @@ while read server; do
   # CREATE Index
   #
   # コピペ時: ここから
-  index_name='idx_ride_statuses_ride_id_created_at'
-  sql="create index ${index_name} on ride_statuses(ride_id, created_at);"
+  index_name='idx_ride_statuses_ride_id_chair_sent_at_created_at'
+  sql="create index ${index_name} on ride_statuses(ride_id, chair_sent_at, created_at);"
   echo "${sql}"
   ssh -n ${server} "sudo mysql ${DB_NAME} -e '${sql}'" || echo "index: ${index_name}は既に有るので問題なし(Duplicate key nameならば)"
   echo ''
